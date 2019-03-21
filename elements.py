fa6e32857bd80d499512afc7eb27ac4a6c7a124a
aldmbelenko <sandcastle.ab@gmail.com>
2019-03-21 18:00:31 +0300

    Broaden this story

--- elements.py
+++ elements.py
@@ -12,19 +12,37 @@ class Answer(object):
 class Page(object):
     def __init__(self, path_to_file):
         with open(path_to_file) as f:
-            data = json.load(f)
-            self.name = data['name']
-            self.text = data['text']
-            self.question = data['question']
-            self.answers = []
-            for answer in data['answers']:
-                self.answers.append(Answer(answer))
+            try:
+                data = json.load(f)
+            except:
+                print(path_to_file)
+                exit(1)
+            try:
+                self.name = data['name']
+                self.text = data['text']
+                self.question = data['question']
+                if 'next_page_name' in data.keys():
+                    self.next_page_name = data['next_page_name']
+                    self.answers = None
+                else:
+                    self.next_page_name = None
+                    self.answers = []
+                    for answer in data['answers']:
+                        self.answers.append(Answer(answer))
+
+            except:
+                print(path_to_file)
+                exit(1)
+
 
 
     def get_name(self):
         return self.name
 
     def next_page(self, usr_ans):
+        if self.next_page_name is not None:
+            return self.next_page_name
+
         for a in self.answers:
             if a.equals(usr_ans):
                 return a.next_page_name
@@ -34,7 +52,10 @@ class Page(object):
         print(self.text)
         print("\n")
         print(self.question)
-        for answer in self.answers:
-            print("\t> " + answer.text)
+        if self.answers is None:
+            print("\t> Нажмите  на любую клавишу")
+        else:
+            for answer in self.answers:
+                print("\t> " + answer.text)
 
 
